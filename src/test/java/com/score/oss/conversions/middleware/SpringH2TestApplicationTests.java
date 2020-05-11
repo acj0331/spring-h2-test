2306363b25cafe88c992f5a5cf162271cd762e8a
changju.ahn <changju.ahn@samsung.com>
2020-05-11 17:40:12 +0900

    add test code(sample)

--- src/test/java/com/score/oss/conversions/middleware/SpringH2TestApplicationTests.java
+++ src/test/java/com/score/oss/conversions/middleware/SpringH2TestApplicationTests.java
@@ -1,13 +1,28 @@
 package com.score.oss.conversions.middleware;
 
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.score.oss.conversions.middleware.repository.UserRepository;
 
 @SpringBootTest
 class SpringH2TestApplicationTests {
+    @Autowired
+    UserRepository userRepo;
+    
 
 	@Test
-	void contextLoads() {
+	void findAllTest() {
+	    Object ret = userRepo.findAll();
+	    assertNotNull(ret);
 	}
 
+
+    @Test
+    void findAdminTest() {
+        Object ret = userRepo.findAdmin();
+        assertNotNull(ret);
+    }
 }
